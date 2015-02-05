76b2b16f8caeab0fb44d5ba06362b31df6553eb6
Katye Russell <katye.russell@gmail.com>
2015-02-05 17:21:43 -0600

    Adding async and additional test modules (with their functions).

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
-/* jshint mocha: true, expr: true, strict: false */
+/* jshint jquery: true */
+/* global async: false */
 
 function addStockToTable(stock) {
   var $row = $('<tr></tr>');
 
   $row.append('<td>' + stock.Name + '</td>');
   $row.append('<td>' + stock.Symbol + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
   $row.append('<td>' + stock.LastPrice + '</td>');
 
   $('tbody').append($row);
@@ -14,14 +16,81 @@ function addStockToTable(stock) {
 
 
 function getStock(symbol, cb) {
-  var url = "http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=" + symbol;
+  var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
 
   $.get(url, function(data) {
     return cb(data);
   }, 'jsonp');
 }
 
+
+function getMultipleStocks(symbols, cb) {
+  async.map(symbols,
+    function (symbol, innercb) {
+      getStock(symbol, function(stock) {
+        innercb(null, stock);
+      });
+     },
+    function (err, stocks) {
+      cb(stocks);
+    }
+  );
+}
+
+
 function hello() {
   return 'world';
 }
+
+
+/*  THIS IS THE NEW FUNCTION BELOW */
+
+
+ var stocks = [
+      { Symbol: 'AAPL', LastPrice: 12.45 },
+      { Symbol: 'MSFT', LastPrice: 23.56 }
+    ];
+
+function refreshStockPrices(obj) {
+   /* for each object, check whether the symbol matches the row */
+
+  .forEach(obj) function ( ) {            }
+
+
+    /* if the symbol matches the row...  The syntax isn't right here. */
+
+    if (obj[i].Symbol) === $trs
+
+    /* replace the text in the 3rd 'td' space with the (obj[i].Symbol) */
+
+       replace ('td')[3].obj[i].LastPrice;
+
+
+/* This is the middle part of the function I used to check the syntax of the object call:  */
+
+ return obj[0].Symbol;
+
+
+/* BELOW HERE IS THE TEST THAT GOES WITH THE FUNCTION WE'RE SUPPOSED TO BE WRITING */
+
+describe('refreshStockPrices', function (){
+  it('should edit each stock in the table with a new price', function () {
+    var stocks = [
+      { Symbol: 'AAPL', LastPrice: 12.45 },
+      { Symbol: 'MSFT', LastPrice: 23.56 }
+    ];
+
+      $tbody = $('tbody'),
+      $trs   = $('tr');
+
+    $('tbody').append('<tr><td> Apple Inc </td><td> 12.45</td><</tr>');
+    $('tbody').append('<tr><td>Microsoft Corp</td><td>23.56</td><</tr>');
+
+    refreshStockPrices(stocks);
+
+    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
+    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
+  });
+});
+
 
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,4 +1,5 @@
-/* jshint mocha: true, expr: true, strict: false */
+/* jshint mocha: true, expr: true, strict: false, undef: false */
+
 
 describe('test suite', function () {
   it('should assert true', function () {
@@ -6,6 +7,7 @@ describe('test suite', function () {
     false.should.be.false;
   });
 });
+
 
 describe('hello', function () {
   it('should return world', function () {
@@ -38,15 +40,34 @@ describe('addStockToTable', function () {
     $('tr').length.should.equal(1);
   });
   it('should use stock data in the appended row', function () {
-    var stock = {Name: "SuperCorp", Symbol: "SCRP", LastPrice: 12.34 },
+    var stock = {Name: "SuperCorp", Symbol: "SCRP", LastPrice: 12.34, LastPrice: 12.34 },
         $row  = addStockToTable(stock),
         $tds  = $row.find('td');
 
-    $tds.length.should.equal(3);
+    $tds.length.should.equal(4);
     $($tds[0]).text().should.equal("SuperCorp");
     $($tds[1]).text().should.equal("SCRP");
     $($tds[2]).text().should.equal("12.34");
+    $($tds[3]).text().should.equal("12.34");
   });
 });
 
+describe('refreshStockPrices', function (){
+  it('should edit each stock in the table with a new price', function () {
+    var stocks = [
+      { Symbol: 'AAPL', LastPrice: 12.45 },
+      { Symbol: 'MSFT', LastPrice: 23.56 }
+    ];
 
+      $tbody = $('tbody'),
+      $trs   = $('tr');
+
+    $('tbody').append('<tr><td> Apple Inc </td><td> 12.45</td><</tr>'); 
+    $('tbody').append('<tr><td>Microsoft Corp</td><td>23.56</td><</tr>');
+
+    refreshStockPrices(stocks);
+
+    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
+    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
+  });
+});
