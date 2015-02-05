1f8dfad1a30a1aee8e00b30f35b4b390b6e5b2b3
Katye Russell <katye.russell@gmail.com>
2015-02-05 10:09:50 -0600

    Adding bower components and test folder.

---
+++ test/spec/main.test.js
@@ -0,0 +1,10 @@
+/* jshint mocha: true, expr: true, strict: false */
+
+describe('test suite', function () {
+  it('should assert true', function () {
+    true.should.be.true;
+    false.should.be.true;
+  });
+});
+
+
