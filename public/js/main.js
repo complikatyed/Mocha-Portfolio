c8537597d092236fae5266a018afcdee2b9efb6e
Katye Russell <katye.russell@gmail.com>
2015-02-06 11:44:08 -0600

    Adding Scott's changes to the testing (from last night).

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
 /* jshint jquery: true */
 /* global async: false */
 
+function refreshStockPrices(stocks) {
+  var $trs = $('tr');
+
+  _.forEach(stocks, function (stock, i) {
+    $($($trs[i]).find('td')[3]).text(stock.LastPrice);
+  });
+}
+
 function addStockToTable(stock) {
   var $row = $('<tr></tr>');
 
@@ -14,83 +22,80 @@ function addStockToTable(stock) {
   return $row;
 }
 
-
 function getStock(symbol, cb) {
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
 
-  $.get(url, function(data) {
-    return cb(data);
+  $.get(url, function (res) {
+    return cb(res);
   }, 'jsonp');
 }
-
 
 function getMultipleStocks(symbols, cb) {
   async.map(symbols,
     function (symbol, innercb) {
-      getStock(symbol, function(stock) {
+      getStock(symbol, function(stock){
         innercb(null, stock);
       });
-     },
-    function (err, stocks) {
+    },
+    function (err, stocks){
       cb(stocks);
     }
   );
-}
-
+ }
 
 function hello() {
   return 'world';
 }
 
 
-/*  THIS IS THE NEW FUNCTION BELOW */
+/*[>  THIS IS THE NEW FUNCTION BELOW <]*/
 
 
- var stocks = [
-      { Symbol: 'AAPL', LastPrice: 12.45 },
-      { Symbol: 'MSFT', LastPrice: 23.56 }
-    ];
+ //var stocks = [
+      //{ Symbol: 'AAPL', LastPrice: 12.45 },
+      //{ Symbol: 'MSFT', LastPrice: 23.56 }
+    //];
 
-function refreshStockPrices(obj) {
-   /* for each object, check whether the symbol matches the row */
+//function refreshStockPrices(obj) {
+   //[> for each object, check whether the symbol matches the row <]
 
-  .forEach(obj) function ( ) {            }
-
-
-    /* if the symbol matches the row...  The syntax isn't right here. */
-
-    if (obj[i].Symbol) === $trs
-
-    /* replace the text in the 3rd 'td' space with the (obj[i].Symbol) */
-
-       replace ('td')[3].obj[i].LastPrice;
+  //.forEach(obj) function ( ) {            }
 
 
-/* This is the middle part of the function I used to check the syntax of the object call:  */
+    //[> if the symbol matches the row...  The syntax isn't right here. <]
 
- return obj[0].Symbol;
+    //if (obj[i].Symbol) === $trs
+
+    //[> replace the text in the 3rd 'td' space with the (obj[i].Symbol) <]
+
+       //replace ('td')[3].obj[i].LastPrice;
 
 
-/* BELOW HERE IS THE TEST THAT GOES WITH THE FUNCTION WE'RE SUPPOSED TO BE WRITING */
+//[> This is the middle part of the function I used to check the syntax of the object call:  <]
 
-describe('refreshStockPrices', function (){
-  it('should edit each stock in the table with a new price', function () {
-    var stocks = [
-      { Symbol: 'AAPL', LastPrice: 12.45 },
-      { Symbol: 'MSFT', LastPrice: 23.56 }
-    ];
+ //return obj[0].Symbol;
 
-      $tbody = $('tbody'),
-      $trs   = $('tr');
 
-    $('tbody').append('<tr><td> Apple Inc </td><td> 12.45</td><</tr>');
-    $('tbody').append('<tr><td>Microsoft Corp</td><td>23.56</td><</tr>');
+//[> BELOW HERE IS THE TEST THAT GOES WITH THE FUNCTION WE'RE SUPPOSED TO BE WRITING <]
 
-    refreshStockPrices(stocks);
+//describe('refreshStockPrices', function (){
+  //it('should edit each stock in the table with a new price', function () {
+    //var stocks = [
+      //{ Symbol: 'AAPL', LastPrice: 12.45 },
+      //{ Symbol: 'MSFT', LastPrice: 23.56 }
+    //];
 
-    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
-    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
-  });
-});
+      //$tbody = $('tbody'),
+      //$trs   = $('tr');
+
+    //$('tbody').append('<tr><td> Apple Inc </td><td> 12.45</td><</tr>');
+    //$('tbody').append('<tr><td>Microsoft Corp</td><td>23.56</td><</tr>');
+
+    //refreshStockPrices(stocks);
+
+    //$($($trs[0]).find('td')[3]).text().should.equal('12.45');
+    //$($($trs[1]).find('td')[3]).text().should.equal('23.56');
+  //});
+//});
 
 
