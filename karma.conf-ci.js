5f2a6678c7acdd14aa30c8fd58e460d141950094
Katye Russell <katye.russell@gmail.com>
2015-02-09 14:46:18 -0600

    Fixing typo.  Also, I hate testing.

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    cutomLaunchers: sauceLaunchers,
+    customLaunchers: sauceLaunchers,
 
     sauceLabs: {
       testName: 'Unit Tests'
